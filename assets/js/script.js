ecc3c7d4516f7baaaf9f7e31011a55f1e5689453
Barry Ferguson <barry@revolvecreative.ie>
2022-12-05 12:52:47 +0000

    Added coded to check answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,8 +37,23 @@ function runGame(gameType) {
     }
 
 }
-
+/**
+ * Checks the answer against the first element in 
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calcualtedAnswer = calculateCorrectAnswer();
+    let isCorrect  = userAnswer === calcualtedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwwww..... you answered ${userAnswer}. The correct answer was ${calcualtedAnswer[0]}!`);
+    }
+
+    runGame(calcualtedAnswer[1]);
 
 }
 
